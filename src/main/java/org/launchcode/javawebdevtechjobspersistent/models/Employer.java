a10b4f993827ab1d639c9a121082afaadd706941
flwrfx <briceflowers@hotmail.com>
2021-03-25 19:58:05 -0500

    the BIG push

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,27 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    private String location;
 
+    @OneToMany
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<>();
+
+    public Employer() {
+    }
+
+    public String getLocation()                 { return location; }
+
+    public void setLocation(String location)    { this.location = location; }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,15 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    private String description;
+
+    public Skill(){
+    }
+
+    public String getDescription(){ return description; }
+    public void setDescription(String skillDescription){ this.description = skillDescription; }
 }
